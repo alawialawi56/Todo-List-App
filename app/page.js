1865dc27182d94d422ded19722b86c5fb7271433
Alawi ALmowalad <alawialmowalad@MacBook-Air-Alawi.local>
2025-08-07 12:31:58 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,179 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import {Box, TextField, Button, List, ListItem, ListItemText, IconButton,Typography, Paper} from '@mui/material';
+import { useEffect, useMemo, useState, useRef, useReducer } from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
+import EditIcon from '@mui/icons-material/Edit';
+import reducer from './reducer/reducer'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todos2,setTodos]= useState([]);
+  const [task,setTask]= useState('');
+  const [filter, setFilter] = useState('all');
+  const [editIndex, setIndex] = useState(null);
+  const [edit, setEdit] = useState('');
+  const [todos, dispatch] = useReducer(reducer, [])
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+
+  const addNewTask = ()=>{
+    dispatch({type:"added", payload:{newTask:task}})
+    if (task.trim() ==='') return;
+      setTask('')
+    };
+  
+  const toggleComplete = (index)=>{
+      dispatch({type:"toggle", payload:{index:index}})
+    };
+
+    const deleteTodos = (index)=>{
+      dispatch({type:"delete", payload:{index:index}})
+    };
+
+    const filteredTodos = useMemo(()=>{
+      return todos.filter((todo)=>{
+        if(filter === "completed") return todo.completed === true;
+        if(filter === "notCompleted") return todo.completed === false;
+        return true;
+    });
+
+    }, [todos,filter])
+
+    const handleEditClick = (index)=>{
+        setIndex(index);
+        setEdit(todos[index].text)
+      }
+
+      const handleEditKeyDown = (event)=>{
+        if (event.key === 'Enter'){
+          if (edit.trim() === '') return;
+          const updateTodos = [...todos];
+          updateTodos[editIndex].text = edit;
+          setTodos(updateTodos)
+          localStorage.setItem("todo", JSON.stringify(updateTodos))
+          setIndex(null)
+          setEdit('')
+          if (event.key === 'Escape') {
+          setIndex(null);
+          setEdit('');
+        }}}
+
+      useEffect(()=>{
+        const showData = JSON.parse(localStorage.getItem("todo"))
+        setTodos(showData)
+      },[])
+
+      const editRef = useRef(null);
+      useEffect(() => {
+        function handleClickOutside(event) {
+          if (editRef.current && !editRef.current.contains(event.target)) {
+            setIndex(null);
+            setEdit('');
+          }
+        }
+
+        if (editIndex !== null) {
+          document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, [editIndex]);
+
+
+  return (
+    <Box sx={{
+      height:'100vh',
+      background:"linear-gradient(to right, #6a11cb, #2575fc)",
+      display:"flex",
+      justifyContent:'center',
+      alignItems:'center',
+      p:2,
+      margin:0,
+      padding:0
+    }}>
+      <Paper elevation={4} sx={{p:4, width:'70%', maxWidth:500, maxHeight:"80vh", overflow:"scroll"}}>
+        <Typography variant='h5' align='left' gutterBottom>
+          TO-Do List
+        </Typography>
+
+        <Box sx={{display:'flex', gap:2, mb:2}}>
+          <TextField
+            fullWidth
+            label="Add a task..."
+            value={task}
+            onChange={(e)=>setTask(e.target.value)}
+            onKeyDown={(e)=>e.key === 'Enter' && addNewTask()}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Button variant='contained' onClick={addNewTask}>Add</Button>
+        </Box>
+
+        <Box sx={{ display:"flex", justifyContent:"center", gap:2, mb:2}}>
+          <Button variant={filter ==='all' ? 'contained' : 'outlined'} onClick={()=> setFilter('all')}>
+            All
+          </Button>
+          <Button variant={filter ==='completed' ? 'contained' : 'outlined'} onClick={()=> setFilter('completed')}>
+            completed
+          </Button>
+          <Button variant={filter ==='notCompleted' ? 'contained' : 'outlined'} onClick={()=> setFilter('notCompleted')}>
+            Not completed
+          </Button>
+        </Box>
+
+        <List>
+          {filteredTodos.map((todo, index)=>(
+            <ListItem
+            key={index}
+            secondaryAction={
+              <>
+                <IconButton onClick={()=>toggleComplete(index)}>
+                  <CheckIcon color={todo.completed ? 'success' : 'disabled'} />
+                </IconButton>
+                <IconButton onClick={()=>deleteTodos(index)}>
+                  <DeleteIcon color="error" />
+                </IconButton>
+                <IconButton onClick={()=>
+                  handleEditClick(index)}>
+                  <EditIcon color="primary"/>
+                </IconButton>
+                
+              </>
+            }
+            >
+              <ListItemText
+              primary={editIndex === index ? (
+                  <Box ref={editRef} sx={{ display: 'flex', gap: 1, alignItems: 'center', flexDirection:"column" }}>
+                    <TextField
+                      value={edit}
+                      onChange={(event) => setEdit(event.target.value)}
+                      onKeyDown={handleEditKeyDown}
+                      size="small"
+                    />
+                    <Button
+                      variant="contained"
+                      size="small"
+                      onClick={() => {
+                        if (edit.trim() === '') return;
+                        const updateTodos = [...todos];
+                        updateTodos[editIndex].text = edit;
+                        setTodos(updateTodos);
+                        setIndex(null);
+                        setEdit('');
+                      }}
+                    >
+                      Save
+                    </Button>
+                  </Box>
+                ) :(
+                  todo.text
+                )}
+                
+              secondary={`Date:${todo.date}`}/>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Box>
   );
 }
---
+++ app/reducer/reducer.js
@@ -0,0 +1,27 @@
+export default function reducer(currentState, action){
+    switch(action.type){
+        case "added":{
+                const newTodos = {
+                    text:action.payload.newTask,
+                    completed:false,
+                    date: new Date().toDateString()
+                }
+                const updatedData = [...currentState,newTodos]
+                localStorage.setItem("todo", JSON.stringify(updatedData))
+                return updatedData
+
+        }case "toggle":{
+            const newTodos = [...currentState];
+            newTodos[action.payload.index].completed = !newTodos[action.payload.index].completed;
+            localStorage.setItem("todo", JSON.stringify(newTodos))
+            return newTodos
+        }case "delete":{    
+            const newTodos = currentState.filter((_, i)=> i !==action.payload.index)
+            localStorage.setItem("todo", JSON.stringify(newTodos))
+            return newTodos
+        }
+        default:{
+            throw Error("Unknown Action " + action.type)
+        }
+    }
+}
